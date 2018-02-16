cd78f002fb4977a742abaa82919f07990efe8761
Jamil Nawaz <jamilnawaz88@gmail.com>
2018-02-16 09:47:48 +0000

    Added label for 'New Match' and implemented dismiss method for view controller

--- SquashMarker/SquashMarker/Controllers/Match Config/MatchSettingsViewController.swift
+++ SquashMarker/SquashMarker/Controllers/Match Config/MatchSettingsViewController.swift
@@ -21,15 +21,8 @@ class MatchSettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissController(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
