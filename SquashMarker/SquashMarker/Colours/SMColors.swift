15d80abaff90013f38a0424f8afe2cc3c060b848
Jamil Nawaz <jamilnawaz88@gmail.com>
2018-02-16 01:28:09 +0000

    Added colours class to project

---
+++ SquashMarker/SquashMarker/Colours/SMColors.swift
@@ -0,0 +1,43 @@
+//
+//  SMColors.swift
+//  SquashMarker
+//
+//  Created by Jamil Nawaz on 16/02/2018.
+//  Copyright © 2018 Jamil Nawaz. All rights reserved.
+//
+
+import UIKit
+
+class SMColors: NSObject {
+
+    class func harvardCrimson() -> UIColor {
+        return UIColor(
+            red: 207.0, green: 0.0, blue: 15.0, alpha: 1.0
+        )
+    }
+    
+    class func antiFlashWhite() -> UIColor {
+        return UIColor(
+            red: 236.0, green: 240.0, blue: 241.0, alpha: 1.0
+        )
+    }
+    
+    class func blackOlive() -> UIColor {
+        return UIColor(
+            red: 57.0, green: 61.0, blue: 63.0, alpha: 1.0
+        )
+    }
+    
+    class func licorice() -> UIColor {
+        return UIColor(
+            red: 28.0, green: 15.0, blue: 19.0, alpha: 1.0
+        )
+    }
+    
+    class func gunmetal() -> UIColor {
+        return UIColor(
+            red: 45.0, green: 49.0, blue: 66.0, alpha: 1.0
+        )
+    }
+    
+}
